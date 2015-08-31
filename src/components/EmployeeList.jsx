1ae1fa26d6d72892da00e63852a46a7c3c49a61e
Emmanuel Morales <emorales0024@gmail.com>
2015-08-30 22:21:13 -0500

    Add icon menu to list for edit and delete

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import BaseComponent from './BaseComponent';
 import _ from 'lodash';
 import api from '../utils/dataService';
-import { Card, CardTitle, List, ListItem, Avatar, FontIcon, IconMenu, MenuItem, IconButton } from 'material-ui';
+import { Card, CardTitle, List, ListItem, Avatar, FontIcon, IconMenu, IconButton } from 'material-ui';
+
+let MenuItem = require('material-ui/lib/menus/menu-item');
+let MenuDivider = require('material-ui/lib/menus/menu-divider');
 
 class EmployeeList extends BaseComponent {
 
   constructor(props) {
     super(props);
-    this._bind('getExternalData', 'renderList');
+    this._bind('getExternalData', 'renderList', 'renderRightMenu');
     this.state = {
     	results: []
     };
@@ -35,10 +38,19 @@ class EmployeeList extends BaseComponent {
   }
 
   renderRightIconButton() {
+  	return (
+			<IconButton iconClassName="fa fa-ellipsis-v" tooltip="Actions"/>
+		);
+  }
+
+  renderRightMenu(result) {
     return (
-      <IconMenu iconButtonElement={<IconButton iconClassName="fa fa-ellipsis-v" tooltip="Actions"/>}>
-        <MenuItem index={0} primaryText="Edit" />
-        <MenuItem index={1} primaryText="Delete" />
+      <IconMenu iconButtonElement={this.renderRightIconButton()}>
+        <MenuItem 
+        	primaryText="Edit"
+        	onClick={this.props.onSelect.bind(this, result)}
+        	/>
+        <MenuItem primaryText="Delete" />
       </IconMenu>
     );
   }
@@ -65,10 +77,9 @@ class EmployeeList extends BaseComponent {
           <ListItem 
             key={result.id} 
             primaryText={result.firstName + ' ' + result.lastName} 
-            onClick={this.props.onSelect.bind(this, result)}
             leftAvatar={this.renderLeftAvatar()}
             secondaryText={this.renderSecondaryText(result)}
-            rigthIconButton={this.renderRightIconButton()}
+            rightIconButton={this.renderRightMenu(result)}
           />
         );
       })
