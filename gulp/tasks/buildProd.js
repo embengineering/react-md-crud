4111f55fe0d162b3f6b249450826bf1178859c5d
Emmanuel Morales <emorales0024@gmail.com>
2016-01-23 19:12:34 -0600

    Another fix for deployment

--- gulp/tasks/buildProd.js
+++ gulp/tasks/buildProd.js
@@ -3,5 +3,5 @@ var runSequence = require('run-sequence');
 
 gulp.task('buildProd', ['clean'], function(callback) {
   callback = callback || function() {};
-  return runSequence(['build', 'sass', 'eslint', 'html'], callback);
+  return runSequence(['build', 'sass', 'eslint', 'html'], 'browserify', callback);
 });
